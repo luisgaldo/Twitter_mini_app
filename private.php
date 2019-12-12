5bf8ce5ece6f64e3a40d671a6cd135b52f23f2e0
Luis Galdo <luis.galdo@gmail.com>
2019-12-13 00:28:58 +0100

    private file where keys must be stored

---
+++ private.php
@@ -0,0 +1,17 @@
+<?php
+function oauth_access_token(){
+    return "";
+}
+
+function oauth_access_token_secret(){
+    return "";
+}
+
+function consumer_key(){
+    return "";
+}
+
+function consumer_secret(){
+    return "";
+}
+?>
